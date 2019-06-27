690c4d6016e81341244f3104961eb3c240d1c8e9
Jorge Aznar <jorgeatgu@gmail.com>
2019-06-27 10:28:36 +0200

    change date()

--- wikipedia-data/s07e05/pageviews-wikipedia.sh
+++ wikipedia-data/s07e05/pageviews-wikipedia.sh
@@ -7,7 +7,7 @@ vamos a obtener solamente la lista de artistas.
 '
 
 # Guardamos en un array la lista de artistas que han participado en el episodio
-readarray -t artist < ~/github/wiki-cachitos/wikipedia-data/s07e01/s07e01.txt
+readarray -t artist < ~/github/wiki-cachitos/wikipedia-data/s07e03/s07e03.txt
 
 type=('mobile-app' 'desktop' 'mobile-web')
 
@@ -16,7 +16,7 @@ for (( i=0; i<${#artist[@]}; ++i )); do
 
     for (( j=0; j<${#type[@]}; ++j )); do
 
-    curl -s "GET" "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/mobile-app/user/"${artist[$i]}"/daily/20190214/20190525" | jq '[.items[] | {"artista": .article, "fecha": .timestamp, "visitas": .views}]' > json/"${artist[$i]}"-"${type[$j]}".json
+    curl -s "GET" "https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/es.wikipedia/mobile-app/user/${artist[$i]}/daily/20190311/20190612" | jq '[.items[] | {"artista": .article, "fecha": .timestamp, "visitas": .views}]' > "${artist[$i]}"-"${type[$j]}".json
 
     done
 
