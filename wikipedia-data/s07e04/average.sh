ad6ad99858c82e6e7b234c8bc8b5dd04afe4d79b
Jorge Aznar <jorgeatgu@gmail.com>
2019-07-02 08:28:32 +0200

    more more more more more more more data

--- wikipedia-data/s07e04/average.sh
+++ wikipedia-data/s07e04/average.sh
@@ -4,10 +4,12 @@
 readarray -t artist < ~/github/wiki-cachitos/wikipedia-data/s07e04/s07e04-list.txt
 
 
+
 for (( i=0; i<${#artist[@]}; ++i )); do
 
     csvgrep -c 1 -r "${artist[$i]}" csv/episodio-cuatro.csv | sed '/2019-06-04/d' | csvstat -c 3 --mean >> csv/mean.csv
 
+    echo "${artist[$i]} terminada!"
 done
 
 
